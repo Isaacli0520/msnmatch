4030bef278362b3ec8c65a9e94bf472f3b6f15b3
Isaac <il5fq@virginia.edu>
2019-05-17 21:40:48 -0400

    change_directory_05

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const BundleTracker = require("webpack-bundle-tracker");
 module.exports = {
     // publicPath: "http://127.0.0.1:8080/",
     // publicPath: "msn-match-test.herokuapp.com/",
+    publicPath: "./dist/",
     outputDir: './dist/',
 
     pages: {
