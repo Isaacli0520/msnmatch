eed90bfb6058c5ee7f76785e480e273f82566e44
Isaac <il5fq@virginia.edu>
2019-05-23 20:42:33 +0800

    debug=false

--- msnmatch/settings.py
+++ msnmatch/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f@hcb!l&kpn_4u+iz)6j4w(5j4$b2!)-=*j(9&(x_0a-j8o6)5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['msn-match-test.herokuapp.com/', 'match.msnatuva.org','localhost']
 
@@ -181,8 +181,8 @@ AWS_S3_OBJECT_PARAMETERS = {
 # STATICFILES_STORAGE = 'msnmatch.storage_backends.StaticStorage'
 DEFAULT_FILE_STORAGE = 'msnmatch.storage_backends.MediaStorage' 
 
-STATIC_HOST = 'https://d1ixiphwkdejqh.cloudfront.net' if not DEBUG else ''
-# STATIC_HOST = ''
+# STATIC_HOST = 'https://d1ixiphwkdejqh.cloudfront.net' if not DEBUG else ''
+STATIC_HOST = ''
 STATIC_URL = STATIC_HOST + '/static/'
 
 
