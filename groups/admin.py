0fd1af27ca14c19638714133634dfb223d7d3a45
Isaac <il5fq@virginia.edu>
2019-05-23 14:28:04 +0800

    group follow admin page

--- groups/admin.py
+++ groups/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Group
+from .models import Group, GroupFollowRelation
 # Register your models here.
     
 class GroupRelationInline(admin.TabularInline):
@@ -8,12 +8,23 @@ class GroupRelationInline(admin.TabularInline):
 class GroupRelationInlineTags(admin.TabularInline):
     model = Group.group_tags.through
 
+class GroupRelationInlineFollow(admin.TabularInline):
+    model = GroupFollowRelation
+
 class GroupAdmin(admin.ModelAdmin):
     list_display = ['group_name', 'group_type']
     list_filter = ('group_type',)
     inlines = [
         GroupRelationInline,
         GroupRelationInlineTags,
+        GroupRelationInlineFollow,
     ]
 
 admin.site.register(Group, GroupAdmin)
+
+
+class GroupFollowingRelationships(admin.ModelAdmin):
+    list_display = ['user', 'group']
+    list_filter = ('group',)
+    
+admin.site.register(GroupFollowRelation, GroupFollowingRelationships)
