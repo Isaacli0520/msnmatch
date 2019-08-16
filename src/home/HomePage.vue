4ef4bf2c60a2422934f3c29f11e88ace7241c6b1
Isaac <il5fq@virginia.edu>
2019-08-16 10:49:37 +0800

    Match Reworked && Credential Updated

--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -1,24 +1,5 @@
 <template>
     <v-app>
-        <v-toolbar clipped-left>
-    <v-toolbar-side-icon  @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
-    <v-toolbar-title>Title</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>Link One</v-btn>
-      <v-btn flat>Link Two</v-btn>
-      <v-btn flat>Link Three</v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
-  
-    <v-navigation-drawer
-      v-model="drawer"
-      temporary
-      absolute
-      width = "200"
-      id = "drawer"
-    >
-    </v-navigation-drawer>
         <v-content>
             <v-container
                 fluid
@@ -57,7 +38,6 @@ import axios from 'axios'
   export default {
     data() {
       return {
-          drawer:null,
           isCollapse:true,
           recognition: '',
           activeIndex: '1',
