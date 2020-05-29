e7114f4afd7914f6615d790c3d7dc1578893e523
Isaac <il5fq@virginia.edu>
2020-05-29 19:40:32 -0400

    super admin update

--- src/superadmin/SuperAdmin.vue
+++ src/superadmin/SuperAdmin.vue
@@ -5,6 +5,30 @@
                 <div class="big-title text-center mt-3 mb-3">
                     <span>Super Cool Admin Page</span>
                 </div>
+                <div class="tmp-span mt-1 mb-1">
+                    Mentors Matched
+                </div>
+                <v-progress-linear
+                    rounded
+                    height = "24"
+                    :value="Math.ceil(mentorMatched / mentorNum)"
+                    color="blue accent-2">
+                    <template v-slot="{ value }">
+                        <strong>{{ mentorMatched }} / {{mentorNum}}</strong>
+                    </template>
+                </v-progress-linear>
+                <div class="tmp-span mt-1 mb-1">
+                    Mentees Matched
+                </div>
+                <v-progress-linear
+                    height = "24"
+                    rounded
+                    :value="Math.ceil(menteeMatched / menteeNum)"
+                    color="green accent-4">
+                    <template v-slot="{ value }">
+                        <strong>{{ menteeMatched }} / {{menteeNum}}</strong>
+                    </template>
+                </v-progress-linear>
                 <v-expansion-panels multiple>
                     <v-expansion-panel :key="user.pk" v-for="user in all_users_list">
                         <v-expansion-panel-header>
@@ -121,11 +145,25 @@ export default {
             all_users_list:[],
         }
     },
+    computed:{
+        mentorNum(){
+            return this.all_users_list.filter(function(user){return user.role == "Mentor"}).length;
+        },
+        menteeNum(){
+            return this.all_users_list.filter(function(user){return user.role == "Mentee"}).length;
+        },
+        mentorMatched(){
+            return this.all_users_list.filter(function(user){return user.role == "Mentor" && user.matched}).length;
+        },
+        menteeMatched(){
+            return this.all_users_list.filter(function(user){return user.role == "Mentee" && user.matched}).length;
+        }
+    },
     methods:{
         matchUser(user_1_pk, user_2_pk){
             this.match_text = "Do you really wanna match "
-             + this.all_users[user_1_pk].first_name + this.all_users[user_1_pk].last_name
-             + " with " + + this.all_users[user_2_pk].first_name + this.all_users[user_2_pk].last_name
+             + this.all_users[user_1_pk].first_name + " " + this.all_users[user_1_pk].last_name
+             + " with " + this.all_users[user_2_pk].first_name + " " + this.all_users[user_2_pk].last_name
              + "?";
             this.user_1_pk = user_1_pk;
             this.user_2_pk = user_2_pk;
@@ -214,6 +252,11 @@ export default {
         font-family: Gill Sans, sans-serif;
         color: #ffffff;
         align-items: center;
+    }
+
+    .tmp-span{
+        font-size: 20px;
+        font-family: Gill Sans, sans-serif;
     }
 
     .user-matched{
