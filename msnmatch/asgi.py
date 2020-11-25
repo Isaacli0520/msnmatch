dd6ef7093bf60aaf61f477d8b6418ef57c76b6a5
Isaac <il5fq@virginia.edu>
2020-11-25 01:55:42 -0500

    asgi fix

--- msnmatch/asgi.py
+++ msnmatch/asgi.py
@@ -1,17 +1,21 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from channels.layers import get_channel_layer
-from channels.auth import AuthMiddlewareStack
 
+from django.core.asgi import get_asgi_application
 import os
 import django
-import comments.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "msnmatch.settings")
+django_asgi_app = get_asgi_application()
+
+from channels.layers import get_channel_layer
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import comments.routing
+
 # django.setup()
-# channel_layer = get_channel_layer()
+channel_layer = get_channel_layer()
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             comments.routing.websocket_urlpatterns
