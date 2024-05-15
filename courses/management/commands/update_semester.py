49469a79742456e118ff2ba2b99489c0e62e4347
Isaac Li <isaac990520li@gmail.com>
2024-05-15 18:30:52 -0400

    update

--- courses/management/commands/update_semester.py
+++ courses/management/commands/update_semester.py
@@ -13,155 +13,153 @@ from msnmatch.utils import cmp_semester
 
 pattern = r'[A-Z]+ \d+-\d+  [A-Z]+ \(\d+\)'
 type_dict = {
-	"IND":"Independent Study",
-	"STO":"Studio",
-	"PRA":"Practicum",
-	"CLN":"Clinical",
-	"SEM":"Seminar",
-	"DRL":"Drill",
-	"WKS":"Workshop",
+  "IND":"Independent Study",
+  "STO":"Studio",
+  "PRA":"Practicum",
+  "CLN":"Clinical",
+  "SEM":"Seminar",
+  "DRL":"Drill",
+  "WKS":"Workshop",
 }
-semester_ids = {
-	"2016Spring":"1162",
-	"2016Fall":"1168",
-	"2017Spring":"1172",
-	"2017Fall":"1178",
-	"2018Spring":"1182",
-	"2018Fall":"1188",
-	"2019Spring":"1192",
-	"2019Fall":"1198",
-	"2020Spring":"1202",
-	"2020Fall":"1208",
-	"2021Spring":"1212",
-	"2021Fall":"1218",
-	"2022Spring":"1222",
-	"2022Fall":"1228"
+form_data = {
+  "iGroup":"", 
+  "iMnemonic":"",
+  "iNumber":"", 
+  "iStatus":"", 
+  "iType":"", 
+  "iInstructor":"", 
+  "iBuilding":"", 
+  "iRoom":"", 
+  "iMode":"", 
+  "iDays":"", 
+  "iTime":"", 
+  "iDates":"", 
+  "iUnits":"", 
+  "iTitle":"", 
+  "iTopic":"", 
+  "iDescription":"", 
+  "iDiscipline":"", 
+  "iMinPosEnroll":"", 
+  "iMaxPosEnroll":"", 
+  "iMinCurEnroll":"", 
+  "iMaxCurEnroll":"", 
+  "iMinCurWaitlist":"", 
+  "iMaxCurWaitlist":"", 
+  "Request CSV Data": "Request CSV Data"
 }
+files = {k: v for k, v in form_data.items()}
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
 
 start_year = 2016
 start_id = 1162
 def calc_semester_id(semester):
-	if re.match(r'2[0-9]{3}(Spring|Fall)$', semester) is None:
-		return None
-	year, season = int(semester[:4]), semester[4:]
-	return start_id + (year - start_year) * 10 + 6 * int(season=="Fall")
+  if re.match(r'2[0-9]{3}(Spring|Fall)$', semester) is None:
+    return None
+  year, season = int(semester[:4]), semester[4:]
+  return start_id + (year - start_year) * 10 + 6 * int(season=="Fall")
 
 
 class Command(BaseCommand):
-	def add_arguments(self, parser):
-		parser.add_argument('semester_id')
+  def add_arguments(self, parser):
+    parser.add_argument('semester_id')
 
-	def handle(self, *args, **kwargs):
-		semester = kwargs["semester_id"]
-		# if semester not in semester_ids:
-		# 	print("Wrong semester")
-		# 	return
-		semester_id = calc_semester_id(semester)
-		if semester_id is None:
-			print("Wrong Semester")
-			return
-		new_data = {
-			"Semester": semester_id,
-            "Group": 'CS',
-            "Description": 'Yes',
-            "submit": 'Submit Data Request',
-            "Extended": 'Yes'
-		}
-		headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
-		data, lines = [], []
-		with requests.post('https://louslist.org//deliverSearchData.php', headers = headers, data=new_data, stream=True) as r:
-			csv_reader = csv.reader(codecs.iterdecode(r.iter_lines(), 'utf-8'))
-			# with open("tmp_csv.csv", 'w') as wf:
-			# 	writer = csv.writer(wf)
-			# 	for row in csv_reader:
-			# 		writer.writerow(row)
-			headers = next(csv_reader) 
-			print("Semester:", semester, "Semester ID:", semester_id)
-			print("headers",headers)
-			print("Comments Before:", CourseUser.objects.all().count())
-			for row in csv_reader:
-				lines.append(row)
-			print("length of lines", len(lines))
-			for i in range(len(lines)): 
-				if re.match(r'\d+',lines[i][0]) == None:
-					lines[i - 1][-1] += lines[i][0]
-					lines[i] = []
-			lines = [line for line in lines if len(line) > 0]
-			for line in lines:
-				tmp_data = {}
-				for i in range(len(line)):
-					tmp_data[headers[i]] = line[i]
+  def handle(self, *args, **kwargs):
+    semester = kwargs["semester_id"]
+    semester_id = calc_semester_id(semester)
+    if semester_id is None:
+      print("Wrong Semester")
+      return
+    
+    # get all courses of a certain semester from louslist
+    lines = []
+    params = {"Semester": semester_id }
+    with requests.post('https://louslist.org//deliverSearchData.php', headers=headers, params=params, files=files, stream=True) as r:
+      csv_reader = csv.reader(codecs.iterdecode(r.iter_lines(), 'utf-8'))
+      headers = next(csv_reader) 
+      print("Semester:", semester, "Semester ID:", semester_id)
+      print("headers",headers)
+      print("Comments Before:", CourseUser.objects.all().count())
+      for row in csv_reader:
+        if len(row) > 0 and row[0].isdigit():
+          lines.append(row)
+      print("length of lines", len(lines))
 
-				# split_description = tmp_data['Description'].split(' ')
-				split_description = re.split(r'\W+', tmp_data['Description'])
-				tmp_data["Prerequisite"] = ""
-				for prereq_key in ["Prerequisite:", "Prerequisites:", "Prerequisite", "Prerequisites"]:
-					if prereq_key in split_description:
-						tmp_data["Prerequisite"] =tmp_data['Description'][(tmp_data['Description'].index(prereq_key) + len(prereq_key)):].strip(':').strip()
-				tmp_data["Semester"] = semester
-				data.append(tmp_data)
-			print("data:",len(data))
+    # store courses' info as a vector of dict
+    data: list[dict] = []
+    for line in lines:
+      tmp_data = {
+        "Semester": semester,
+        "Prerequisite": ""
+      }
+      for i in range(len(line)):
+        tmp_data[headers[i]] = line[i]
 
-		old_cs_instrs = CourseInstructor.objects.filter(semester = semester)
-		old_cs_instrs_dict = {}
-		for old_cs_instr in old_cs_instrs:
-			old_cs_instrs_dict[old_cs_instr] = 1
-		 
-		for cs in data:
-			if cs["Type"] in type_dict:
-				cs['Type'] = type_dict[cs['Type']]
+      # extract prereq if possible
+      split_description = re.split(r'\W+', tmp_data['Description'])
+      for prereq_key in ["Prerequisite:", "Prerequisites:", "Prerequisite", "Prerequisites"]:
+        if prereq_key in split_description:
+          tmp_data["Prerequisite"] =tmp_data['Description'][(tmp_data['Description'].index(prereq_key) + len(prereq_key)):].strip(':').strip()
+      data.append(tmp_data)
+    print("courses size:", len(data))
 
-			try:
-				course = Course.objects.get(number=cs['Number'], mnemonic=cs['Mnemonic'], title=cs['Title'], type=cs['Type'])
-				course.units = cs["Units"]
-				course.description = cs["Description"]
-				course.prerequisite = cs["Prerequisite"]
-				course.save()
-			except Course.DoesNotExist:
-				course = Course.objects.create(number=cs['Number'], mnemonic=cs['Mnemonic'], units=cs['Units'], title=cs['Title'],description=cs["Description"], type=cs['Type'], prerequisite=cs['Prerequisite'])
-			
-			tbd_instructors = []
-			final_instructors = []
-			for i in range(1, 5):
-				tmp_instructor = cs["Instructor"+str(i)]
-				if tmp_instructor != "":
-					if len(tmp_instructor.split(',')) > 1:
-						for instructor in tmp_instructor.split(','):
-							final_instructors.append(instructor.strip().split())
-					else:
-						final_instructors.append(tmp_instructor.strip().split())
+    old_cs_instrs = CourseInstructor.objects.filter(semester = semester)
+    old_cs_instrs_dict = {old_cs_instr : 1 for old_cs_instr in old_cs_instrs}
+     
+    for cs in data:
+      # transform acronym e.g. SEM to Seminar
+      if cs["Type"] in type_dict:
+        cs['Type'] = type_dict[cs['Type']]
 
-			# ['ClassNumber', 'Mnemonic', 'Number', 'Section', 'Type', 
-			# 'Units', 'Instructor1', 'Days1', 'Room1', 'MeetingDates1', 
-			# 'Instructor2', 'Days2', 'Room2', 'MeetingDates2', 'Instructor3', 
-			# 'Days3', 'Room3', 'MeetingDates3', 'Instructor4', 'Days4', 'Room4'
-			# , 'MeetingDates4', 'Title', 'Topic', 'Status', 'Enrollment', 'EnrollmentLimit',
-			#  'Waitlist', 'Description']
+      # update or create course
+      try:
+        course = Course.objects.get(number=cs['Number'], mnemonic=cs['Mnemonic'], title=cs['Title'], type=cs['Type'])
+        course.units = cs["Units"]
+        course.description = cs["Description"]
+        course.prerequisite = cs["Prerequisite"]
+        course.save()
+      except Course.DoesNotExist:
+        course = Course.objects.create(number=cs['Number'], mnemonic=cs['Mnemonic'], units=cs['Units'], title=cs['Title'],description=cs["Description"], type=cs['Type'], prerequisite=cs['Prerequisite'])
+      
+      # instructors can be multiple
+      final_instructors = []
+      split_instructors = cs["Instructor(s)"].split(',')
+      for instructor in split_instructors:
+        # first name + last name should be at least 3 chars
+        if len(instructor.strip()) > 2:
+          final_instructors.append(instructor.strip().split())
 
-			for instructor in final_instructors:
-				if len(instructor) == 0:
-					print("Empty Instructor Name")
-					return
-				tmp_first_name = instructor[0]
-				tmp_last_name = "" if len(instructor) == 1 else instructor[-1]
+      # ['ClassNumber', 'Mnemonic', 'Number', 'Section', 'Type', 
+      # 'Units', 'Instructor1', 'Days1', 'Room1', 'MeetingDates1', 
+      # 'Instructor2', 'Days2', 'Room2', 'MeetingDates2', 'Instructor3', 
+      # 'Days3', 'Room3', 'MeetingDates3', 'Instructor4', 'Days4', 'Room4'
+      # , 'MeetingDates4', 'Title', 'Topic', 'Status', 'Enrollment', 'EnrollmentLimit',
+      #  'Waitlist', 'Description']
 
-				try:
-					instr = Instructor.objects.get(first_name=tmp_first_name, last_name=tmp_last_name)
-				except Instructor.DoesNotExist:
-					instr = Instructor.objects.create(first_name=tmp_first_name, last_name=tmp_last_name)
-				
-				try:
-					cs_instr = CourseInstructor.objects.get(instructor=instr, course=course, topic=cs["Topic"], semester=cs["Semester"])
-					old_cs_instrs_dict[cs_instr] = 0
-				except CourseInstructor.DoesNotExist:
-					print("NEW Course Instructor Relation found -", "Instructor:",instr.first_name,instr.last_name, "Course:", course.mnemonic, course.number,)
-					cs_instr = CourseInstructor.objects.create(instructor=instr, course=course, topic=cs["Topic"], semester=cs["Semester"])
-				
-		for old_cs_instr, v in old_cs_instrs_dict.items():
-			try:
-				if v == 1 and not CourseUser.objects.filter(course_instructor=old_cs_instr).first():
-					print("Old Course Instructor Relation Deleted -", "Instructor:",old_cs_instr.instructor.first_name, old_cs_instr.instructor.last_name, "Course:", old_cs_instr.course.mnemonic, old_cs_instr.course.number, old_cs_instr.course.title)
-					old_cs_instr.delete()
-			except Instructor.DoesNotExist:
-				print("OLD instructor error")
-		print("Comments After:", CourseUser.objects.all().count())
+      for instructor in final_instructors:
+        if len(instructor) == 0:
+          print("Empty Instructor Name")
+          continue
+        tmp_first_name = instructor[0]
+        tmp_last_name = "" if len(instructor) == 1 else instructor[-1]
+
+        try:
+          instr = Instructor.objects.get(first_name=tmp_first_name, last_name=tmp_last_name)
+        except Instructor.DoesNotExist:
+          instr = Instructor.objects.create(first_name=tmp_first_name, last_name=tmp_last_name)
+        
+        try:
+          cs_instr = CourseInstructor.objects.get(instructor=instr, course=course, topic=cs["Topic"], semester=cs["Semester"])
+          old_cs_instrs_dict[cs_instr] = 0
+        except CourseInstructor.DoesNotExist:
+          print("NEW Course Instructor Relation found -", "Instructor:",instr.first_name,instr.last_name, "Course:", course.mnemonic, course.number,)
+          cs_instr = CourseInstructor.objects.create(instructor=instr, course=course, topic=cs["Topic"], semester=cs["Semester"])
+
+    # remove course-instructor relation that is not seen in this semester
+    for old_cs_instr, v in old_cs_instrs_dict.items():
+      try:
+        if v == 1 and not CourseUser.objects.filter(course_instructor=old_cs_instr).first():
+          print("Old Course Instructor Relation Deleted -", "Instructor:",old_cs_instr.instructor.first_name, old_cs_instr.instructor.last_name, "Course:", old_cs_instr.course.mnemonic, old_cs_instr.course.number, old_cs_instr.course.title)
+          old_cs_instr.delete()
+      except Instructor.DoesNotExist:
+        print("OLD instructor error")
+    print("Comments After:", CourseUser.objects.all().count())
